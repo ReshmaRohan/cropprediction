887c25113b270c48d2bc50be80fe56b03adf9971
ReshmaRohan <47878217+ReshmaRohan@users.noreply.github.com>
2019-03-29 09:49:14 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -34,3 +34,10 @@ class Agridata(models.Model):
 	
 	def __str__(self):
 		return self.state_name+'-'+self.district_name
+class Contact(models.Model):
+	name=models.CharField(max_length=30)
+	email = models.EmailField(max_length=30)
+	message = models.CharField(max_length=80)
+
+	def __str__(self):
+		return str(self.name)
