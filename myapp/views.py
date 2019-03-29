4e3f6c1cea54058915dc33e7b8d46381a298dae4
ReshmaRohan <47878217+ReshmaRohan@users.noreply.github.com>
2019-03-29 09:45:20 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -152,6 +152,18 @@ def About(request):
 	return render(request, 'about.html', {})
 
 
+def contact(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        message=request.POST.get('message')
+        s=Contact(name=name,email=email,message=message)
+        s.save()
+        return render(request,'contact.html',{'message':"hloo"})
+
+    else:
+        return render(request,'contact.html',{})    
+
 def Cluster(request):
 	data = Agromain.objects.filter(active=True)[0]
 	filename = "{}/media/{}".format(BASE_DIR,data.data_file)
