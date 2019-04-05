a3a0a3460769c78ed77f687c5c875d2bc773e5d7
ReshmaRohan <47878217+ReshmaRohan@users.noreply.github.com>
2019-04-05 10:07:01 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
    url(r'^logout/',views.log_out,name='logout_url'),
    url(r'^Clustering/',views.cluster,name='cluster_url'),
    url(r'^prediction/',views.prediction,name='predict_url'),
+   url(r'^show/',views.show,name='show_url'),
    url(r'^contact/',views.contact,name='contact_url'),
    url(r'^About/',views.About,name='about_url'),
   
